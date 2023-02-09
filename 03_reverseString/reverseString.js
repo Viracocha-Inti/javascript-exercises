f668b2acb54ccc8cd6dd73f0701027ebfb275d82
Viracocha-Inti <valenciasamir21@gmail.com>
2023-02-09 16:04:06 -0500

    Add own solutions to EX 3 & 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let word = string;
+  let spread = [];
+  let backward = [];
+  let backwardWord;
+  spread = word.split("");
+  backward = spread.reverse();
+  backwardWord = backward.join("");
+  return backwardWord;
 };
+
+// const reverseString = function(string) {
+//     return string.split('').reverse().join('');
+//    };
 
 // Do not edit below this line
 module.exports = reverseString;
