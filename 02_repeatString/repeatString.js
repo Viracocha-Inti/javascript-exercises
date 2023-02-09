dc3e7e00dee071bf1842cf63c1443a4abede18e8
Viracocha-Inti <valenciasamir21@gmail.com>
2023-02-09 12:02:19 -0500

    Add my own solutions to EX: 1 & 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (word, times) {
+  if (times < 0) {
+    let string = "ERROR";
+    return string;
+  }
+  let string = word.repeat(times);
+  return string;
 };
 
 // Do not edit below this line
