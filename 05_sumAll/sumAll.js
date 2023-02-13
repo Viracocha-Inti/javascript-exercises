5221d25586b1347c59ced3b049e794fbc679446f
Viracocha-Inti <valenciasamir21@gmail.com>
2023-02-12 21:46:59 -0500

    Change below
    Changes to be committed:
    	modified:   05_sumAll/sumAll.js
    	modified:   05_sumAll/sumAll.spec.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  let sum = 0;
+  if (num2 < 0 || num1 < 0) {
+    return "ERROR";
+  }
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+    return "ERROR";
+  }
+  if (num1 > num2) {
+    const realMax = num1;
+    num1 = num2;
+    num2 = realMax;
+  }
+  for (i = num1; i <= num2; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
